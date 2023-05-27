6f0be1c2d9a94ab02de5d8058a78041457a09257
jyh4479 <jyh.dev@gmail.com>
2023-05-27 22:06:29 +0900

    [FIX] 테스트 스펙 변경
    - 단순 API 테스트에서 React query를 이용한 테스트로 변경

---
+++ src/hooks/serverStateHooks/useTodoList/useTodoListQuery.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import {BASE_URL} from "../../../commons/constants";
+import {useQuery} from "@tanstack/react-query";
+
+const API_KET = 'posts';
+const fetchTodoList = async () => {
+    const response = await axios.get(BASE_URL + API_KET);
+    return response.data;
+}
+
+export const useTodoListQuery = () => {
+    return useQuery({queryKey: [API_KET], queryFn: fetchTodoList});
+};

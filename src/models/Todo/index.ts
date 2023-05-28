afa6e094ed05c4d0319ef22fc740ea2d4ab4a10a
jyh4479 <jyh.dev@gmail.com>
2023-05-28 17:38:05 +0900

    [ADD] Todo model

---
+++ src/models/Todo/index.ts
@@ -0,0 +1,23 @@
+interface TodoInterface {
+    id: number;
+    userId: number;
+    title: string;
+    body: string;
+}
+
+export class Todo implements TodoInterface {
+    id: number;
+    userId: number;
+    title: string;
+    body: string;
+
+    constructor(id: number, userId: number, title: string, body: string) {
+        this.id = id;
+        this.userId = userId;
+        this.title = title;
+        this.body = body;
+    }
+}
+
+export type CreateTodoModel = Omit<TodoInterface, 'id'>;
+export type UpdateTodoModel = TodoInterface;

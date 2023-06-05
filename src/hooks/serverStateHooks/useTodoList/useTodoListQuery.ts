81da36e5f710d5358842205dc01cdc0ba08cadef
jyh4479 <jyh.dev@gmail.com>
2023-06-05 21:34:10 +0900

    [FEAT] React query config

--- src/hooks/serverStateHooks/useTodoList/useTodoListQuery.ts
+++ src/hooks/serverStateHooks/useTodoList/useTodoListQuery.ts
@@ -1,13 +1,14 @@
 import axios from 'axios';
-import {BASE_URL} from "../../../commons/constants";
-import {useQuery} from "@tanstack/react-query";
+import { BASE_URL } from '../../../commons/constants';
+import { useQuery } from '@tanstack/react-query';
 
 const API_KET = 'posts';
+
 const fetchTodoList = async () => {
-    const response = await axios.get(BASE_URL + API_KET);
-    return response.data;
-}
+  const response = await axios.get(BASE_URL + API_KET);
+  return response.data;
+};
 
 export const useTodoListQuery = () => {
-    return useQuery({queryKey: [API_KET], queryFn: fetchTodoList});
+  return useQuery({ queryKey: [API_KET], queryFn: fetchTodoList });
 };

d5509e735db4092fdb994b51caf242e3d3a63ceb
jyh4479 <jyh.dev@gmail.com>
2023-06-08 19:11:10 +0900

    [FEAT] Create todo

--- src/containers/TodoListCreateModalContainer/index.tsx
+++ src/containers/TodoListCreateModalContainer/index.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
-import { Modal } from 'antd';
+import React, { useState } from 'react';
+import { Modal, Input } from 'antd';
+import { useTodoListCreateMutation } from '../../hooks/serverStateHooks/useTodoList/useTodoListMutation';
 
 const TodoListCreateModalContainer = (props) => {
+  const { mutate } = useTodoListCreateMutation();
+
   const { modalHandler } = props;
 
-  return <Modal {...modalHandler}>Modal Test</Modal>;
+  const [id, setId] = useState(0);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  return (
+    <Modal
+      {...modalHandler}
+      onOk={() =>
+        mutate({
+          userId: id,
+          title: title,
+          body: body,
+        })
+      }
+    >
+      <Input placeholder={'ID'} onChange={(e) => setId(Number(e.target.value))} />
+      <Input placeholder={'Title'} onChange={(e) => setTitle(e.target.value)} />
+      <Input placeholder={'Body'} onChange={(e) => setBody(e.target.value)} />
+    </Modal>
+  );
 };
 
 export default TodoListCreateModalContainer;
